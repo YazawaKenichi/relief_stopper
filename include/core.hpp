df43e980b601dcf23d94ad074c9e2ed820410e25
YazawaKenichi <tonfarken83825@gmail.com>
2025-01-18 21:06:49 +0900

    hoge

--- include/core.hpp
+++ include/core.hpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
-#include "std_msgs/msg/bool.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 #include "FileRW.hpp"
 #include "TimeObserver.hpp"
@@ -20,15 +20,20 @@ public:
     ReliefStopper();
 
 private:
-    void timer_cb();
-    void bar_callback(const std_msgs::msg::Float32 &) const;
+    void raw_cmd_vel_callback_(const geometry_msgs::msg::Twist & raw_cmd_vel);
+    void bar_angle_callback_(const std_msgs::msg::Float32 & angle);
 
-    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr stopping_publisher_;
-    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr bar_value_subscriber_;
-    std::string stop_lifecycle_name_;
-    std::string sub_topic_name_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr processed_cmd_vel_publisher_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr raw_cmd_subscriber_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr bar_angle_subscriber_;
+    std::string processed_cmd_vel_name_;
+    std::string raw_cmd_vel_name_;
+    std::string bar_angle_name_;
+    geometry_msgs::msg::Twist cmd_vel_zeros_;
+
     float relief_threshold_;
     bool reverse_;
+    bool stopping_;
 
     std::string saving_raw_data_path_;
     std::string saving_stopping_data_path_;
--- src/core.cpp
+++ src/core.cpp
@@ -4,16 +4,21 @@
 #include "core.hpp"
 #include "FileRW.hpp"
 #include "TimeObserver.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 namespace ReliefStoppers
 {
 //! コンストラクタ
 ReliefStopper::ReliefStopper()
     : Node("relief_stopper"),
-    stop_lifecycle_name_("/relief_stopper/bar/stopping"),
-    sub_topic_name_("/relief_detector/bar/raw"),
+    processed_cmd_vel_name_("/cmd_vel"),
+    raw_cmd_vel_name_("/cmd_vel_raw"),
+    bar_angle_name_("/bar/angle"),
     relief_threshold_(1.0f),
     reverse_(false),
+    stopping_(false),
     saving_raw_data_path_("/dev/null"),
     saving_stopping_data_path_("/dev/null"),
     raw_data_file_(this->saving_raw_data_path_),
@@ -27,12 +32,14 @@ ReliefStopper::ReliefStopper()
 
     ////////// パラメータ //////////
     //! 定義
-    this->declare_parameter("stopping_lifecycle_name", this->stop_lifecycle_name_);
-    this->declare_parameter("sub_topic_name", this->sub_topic_name_);
+    this->declare_parameter("processed_cmd_vel_topic_name", this->processed_cmd_vel_name_);
+    this->declare_parameter("raw_cmd_vel_topic_name", this->raw_cmd_vel_name_);
+    this->declare_parameter("bar_angle_topic_name", this->bar_angle_name_);
     this->declare_parameter("relief_threshold", this->relief_threshold_);
     //! 読み込み
-    this->stop_lifecycle_name_ = this->get_parameter("stopping_lifecycle_name").as_string();
-    this->sub_topic_name_ = this->get_parameter("sub_topic_name").as_string();
+    this->processed_cmd_vel_name_ = this->get_parameter("processed_cmd_vel_topic_name").as_string();
+    this->raw_cmd_vel_name_ = this->get_parameter("raw_cmd_vel_topic_name").as_string();
+    this->bar_angle_name_ = this->get_parameter("bar_angle_topic_name").as_string();
     this->relief_threshold_ = this->get_parameter("relief_threshold").as_double();
 
     ////////// 記録 //////////
@@ -44,34 +51,47 @@ ReliefStopper::ReliefStopper()
     this->raw_data_file_ = FileRW::FileRW(this->saving_raw_data_path_);
     this->stopping_data_file_ = FileRW::FileRW(this->saving_stopping_data_path_);
 
+    ////////// 初期値の代入 //////////
+    this->cmd_vel_zeros_.linear.x = 0.0f;
+    this->cmd_vel_zeros_.linear.y = 0.0f;
+    this->cmd_vel_zeros_.linear.z = 0.0f;
+    this->cmd_vel_zeros_.angular.x = 0.0f;
+    this->cmd_vel_zeros_.angular.y = 0.0f;
+    this->cmd_vel_zeros_.angular.z = 0.0f;
+
     ////////// パブリッシャ //////////
-    this->stopping_publisher_ = this->create_publisher<std_msgs::msg::Bool>(this->stop_lifecycle_name_, 10);
+    this->processed_cmd_vel_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(this->processed_cmd_vel_name_, 10);
 
     ////////// サブスクライバ //////////
-    this->bar_value_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
-            this->sub_topic_name_, 10, std::bind(
-                &ReliefStopper::bar_callback, this,
+    this->raw_cmd_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            this->raw_cmd_vel_name_, 10, std::bind(
+                &ReliefStopper::raw_cmd_vel_callback_, this,
+                std::placeholders::_1));
+    this->bar_angle_subscriber_ = this->create_subscription<std_msgs::msg::Float32>(
+            this->bar_angle_name_, 10, std::bind(
+                &ReliefStopper::bar_angle_callback_, this,
                 std::placeholders::_1));
 }
 
-//! サブスクライブ コールバック
-void ReliefStopper::bar_callback(const std_msgs::msg::Float32 & raw) const
+void ReliefStopper::raw_cmd_vel_callback_(const geometry_msgs::msg::Twist & raw_cmd_vel)
 {
-    //! サブスクライブ値の範囲
-    // const int max = 256;
-    // const int min = 0;
+    if(this->stopping_)
+    {
+        this->processed_cmd_vel_publisher_->publish(this->cmd_vel_zeros_);
+    }
+    else
+    {
+        this->processed_cmd_vel_publisher_->publish(raw_cmd_vel);
+    }
+}
 
-    //! 変換後の値
-    std_msgs::msg::Bool stopping;
-    stopping.data = (!this->reverse_) ? (raw.data > this->relief_threshold_ ? true : false) : (raw.data > this->relief_threshold_ ? false : true);
+//! サブスクライブ コールバック
+void ReliefStopper::bar_angle_callback_(const std_msgs::msg::Float32 & angle)
+{
+    this->stopping_ = (!this->reverse_) ? (angle.data > this->relief_threshold_ ? true : false) : (angle.data > this->relief_threshold_ ? false : true);
 
-    RCLCPP_INFO(this->get_logger(), "    raw  : %8.3f", raw.data);
-    RCLCPP_INFO(this->get_logger(), "stopping : %8d", stopping.data ? 1 : 0);
-
-    this->raw_data_file_.writef(std::to_string(raw.data) + "\r\n");
-    this->stopping_data_file_.writef(std::to_string(stopping.data) + "\r\n");
-
-    this->stopping_publisher_->publish(stopping);
+    RCLCPP_INFO(this->get_logger(), "    raw  : %8.3f", angle.data);
+    RCLCPP_INFO(this->get_logger(), "stopping : %8d", this->stopping_ ? 1 : 0);
 }
 }
 
